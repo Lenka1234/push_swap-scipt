2f78c3f7b62f5ce5e50f001f7e00f980215e1c22
Lenka Husárová <lenka@husarova.cz>
2025-10-15 14:13:40 +0200

    added checking 5 nums

--- script.sh
+++ script.sh
@@ -1,9 +1,40 @@
 #!/usr/bin/env bash
 
 COUNT=10  # how many times to run the program
-SIZE=100  # how many numbers to generate
+SIZE=5  # how many numbers to generate
 SUM=0     # total number of instructions
 MAX=0     # worst case (maximum instructions)
+
+echo "5 numbers"
+for i in $(seq 1 $COUNT)
+do
+    ARG=$(python3 generate.py $SIZE)
+
+    echo "Test $i:"
+    ./push_swap $ARG | ./checker_linux $ARG
+
+    INSTR=$(./push_swap $ARG | wc -l)
+    echo "Number of instructions: $INSTR"
+    echo
+    SUM=$((SUM + INSTR))
+    if [ $INSTR -gt $MAX ]; then
+        MAX=$INSTR
+    fi
+done
+
+AVG=$((SUM / COUNT))
+
+echo "=========================="
+echo "Worst case (max): $MAX"
+echo "Average number of instructions: $AVG"
+
+echo
+echo "=========================="
+
+
+SIZE=100
+SUM=0
+MAX=0
 
 for i in $(seq 1 $COUNT)
 do
